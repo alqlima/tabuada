921340bfa2ac7ce5dfe597a6ec2655be9ef7f41d
André Queiroz <alqlima@gmail.com>
2020-09-07 09:24:32 -0300

    Update script.js
    It was added a function to calculate the multiplication table

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
+function calculaTabuada() {
+	let tabuada = document.querySelector("#tabuada tbody");
 
-let tabuada = document.querySelector("#tabuada tbody");
+	let valorA = 99;
 
-let valorA = 99;
+	tabuada.innerHTML = '';
 
-tabuada.innerHTML = '';
-
-for (let valorB = 0; valorB <= 10; valorB++) {
+	for (let valorB = 0; valorB <= 10; valorB++) {
 	
-	let resultado = valorA * valorB;
+		let resultado = valorA * valorB;
 	
-	let template = `<td>${valorA}</td><td>x</td><td>${valorB}</td><td>=</td><td>${resultado}</td>`;
+		let template = `<td>${valorA}</td><td>x</td><td>${valorB}</td><td>=</td><td>${resultado}</td>`;
 				
-let tr = document.createElement('tr');
+	let tr = document.createElement('tr');
 
-tr.innerHTML = template;
+	tr.innerHTML = template;
 
-tabuada.append(tr);
+	tabuada.append(tr);
 };
+};
+calculaTabuada();
